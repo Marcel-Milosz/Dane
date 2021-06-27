fab5b7849686c18dfbda5e0a42a19c2e2a35de2b
unknown <mmilosz@edu.cdv.pl>
2021-06-27 12:19:30 +0200

    anscobe update

--- Milosz_Marcel_anscobe_niestacj.py
+++ Milosz_Marcel_anscobe_niestacj.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-x  = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
+x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
 y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
 y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
 y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
 x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
 y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
 
-df_dict = {
-    'I': y1,
-    'II': y2,
-    'III': y3,
-    'IV': y4
-}
+df_dict = {"I": y1, "II": y2, "III": y3, "IV": y4}
 
 anscombe_df = pd.DataFrame(df_dict, index=x)
 
-def funcA() :
-    ''' Returns 4 plots using anscobe_df '''
-    
+
+def funcA():
+    """Returns 4 plots using anscobe_df"""
+
     scatter_plot_df = anscombe_df.reset_index()
-    fig, axs = plt.subplots(2,2, sharex=True, sharey=True, figsize=(6, 6))
+    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True, figsize=(6, 6))
     axs[0, 0].set(xlim=(0, 20), ylim=(2, 14))
     axs[0, 0].set(xticks=(0, 10, 20), yticks=(4, 8, 12))
     axs[0, 0].scatter(x, y1)
@@ -30,7 +26,19 @@ def funcA() :
     axs[1, 1].scatter(x4, y4)
     fig.tight_layout()
 
-    plt.savefig('output/plot.png')
+    plt.savefig("output/plot.png")
 
-if __name__ == '__main__':
+
+def saveCsv():
+    """Saving data to csv"""
+    
+    df = pd.DataFrame(
+        [x, x4, y1, y2, y3, y4], index=["x", "x4", "y1", "y2", "y3", "y4"]
+    )
+    print(df)
+    df.to_csv("output/data.csv", index=False)
+
+
+if __name__ == "__main__":
     funcA()
+    saveCsv()
